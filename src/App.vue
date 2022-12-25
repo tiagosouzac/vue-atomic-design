f8a1cffc811854bd224f135de8e97ae71c565190
Tiago Castro <tiagocastro602@gmail.com>
2022-12-24 21:15:26 -0300

    logo atom

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
-<template>
-  <h1>Hello, World!</h1>
-</template>
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Logo } from '@/components/atoms/index'
+
+export default defineComponent({
+  components: { Logo },
+})
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'normalize.css'
-import '@/styles/main.css'
+import './styles/main.css'
 
 createApp(App).mount('#app')
