bf4425df1f60cb20e89726f9d0eb2084b4345482
Tiago Castro <tiagocastro602@gmail.com>
2022-12-24 23:24:44 -0300

    social link atom

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <SearchForm />
+  <SocialLink name="Github" to="https://github.com" icon="github.svg" />
+  <SocialLink name="Twitter" to="https://twitter.com" icon="twitter.svg" />
+  <SocialLink name="Facebook" to="https://facebook.com" icon="facebook.svg" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { SearchForm } from '@/components/molecules'
+import { SocialLink } from '@/components/atoms'
 
 export default defineComponent({
-  components: { SearchForm },
+  components: { SocialLink },
 })
 </script>
---
+++ src/components/atoms/SocialLink.vue
@@ -0,0 +1,54 @@
+<template>
+  <a class="social-link" :href="to" target="_blank">
+    <img :src="iconPath" :alt="name" />
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    iconPath(): string {
+      const path = `../../assets/images/icons/${this.icon}`
+      return new URL(path, import.meta.url).href
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.social-link {
+  width: 3rem;
+  height: 3rem;
+  border-radius: 3rem;
+  background-color: #ff895f;
+  transition: transform 0.2s linear;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+
+  img {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+}
+</style>
