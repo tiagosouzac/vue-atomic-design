f033a91577eb43e7eff2453bb10eafabdd6bcb28
Tiago Castro <tiagocastro602@gmail.com>
2022-12-24 23:28:09 -0300

    social networks molecule

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <SocialLink name="Github" to="https://github.com" icon="github.svg" />
-  <SocialLink name="Twitter" to="https://twitter.com" icon="twitter.svg" />
-  <SocialLink name="Facebook" to="https://facebook.com" icon="facebook.svg" />
+  <SocialNetworks />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { SocialLink } from '@/components/atoms'
+import { SocialNetworks } from '@/components/molecules'
 
 export default defineComponent({
-  components: { SocialLink },
+  components: { SocialNetworks },
 })
 </script>
