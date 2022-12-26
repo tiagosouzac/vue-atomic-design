7ecc1f7059e514b4caccd8dac3c1a23421f3f1b4
Tiago Castro <tiagocastro602@gmail.com>
2022-12-25 21:46:31 -0300

    service molecule

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <SocialNetworks />
+  <Service
+    title="Front end"
+    content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, amet harum esse quibusdam ratione excepturi sunt libero, laboriosam delectus laborum aspernatur incidunt voluptatibus eligendi porro! Nihil, minus quibusdam. Consequatur, rem?"
+  />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { SocialNetworks } from '@/components/molecules'
+import { Service } from '@/components/molecules'
 
 export default defineComponent({
-  components: { SocialNetworks },
+  components: { Service },
 })
 </script>
---
+++ src/components/molecules/Service.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="service">
+    <h2>{{ title }}</h2>
+    <p>{{ content }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.service {
+  h2 {
+    margin-bottom: 0.8rem;
+    color: #935e92;
+    font-weight: bold;
+  }
+
+  p {
+    color: #293947;
+    line-height: 1.5;
+  }
+}
+</style>
