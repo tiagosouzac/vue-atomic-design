1b95be08412ff8f6b4b2daf4e359ffe6e542d617
Tiago Castro <tiagocastro602@gmail.com>
2022-12-25 22:08:22 -0300

    container boson

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <Header />
-  <Presentation />
-  <Services />
+  <Container>
+    <Header />
+    <Presentation />
+    <Services />
+  </Container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { Header, Presentation, Services } from '@/components/organisms'
+import { Container } from '@/components/bosons'
 
 export default defineComponent({
-  components: { Header, Presentation, Services },
+  components: { Header, Presentation, Services, Container },
 })
 </script>
